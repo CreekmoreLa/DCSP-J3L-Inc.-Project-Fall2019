84ab4eda03f4597b89a7a84ab3760c9dceb40ff6
joepatt98 <joepatterson98@icloud.com>
2019-11-15 13:27:50 -0600

    added logout_page.php

---
+++ logout_page.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Logged Out</title>
+    </head>
+    <?php
+
+        // remove session and session cookie
+        session_start();
+
+        setcookie('User', null, -1);
+        setcookie('Admin', null, -1);
+
+
+        session_unset();
+        session_destroy();
+
+    ?>
+    <body>
+        <h1>Logged Out</h1>
+        <p>
+            You are now logged out of the website.
+        </p>
+        <p>
+            <a href="login_page.php">Log in</a> again.
+        </p>
+    </body>
+</html>
