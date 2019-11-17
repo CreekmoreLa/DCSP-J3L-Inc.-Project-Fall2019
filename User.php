371b7184665c50ba07f9835c94adea27e32c2445
CreekmoreLa <56848353+CreekmoreLa@users.noreply.github.com>
2019-11-16 19:57:51 -0600

    Labeled what to link create account to.

--- User.php
+++ User.php
@@ -4,30 +4,25 @@ class user() {
 
 
 
-public function login()
-{
+  public function login(){
 
-}
+  }
 
-public function logout()
-{
+  public function logout(){
 
-}
+  }
 
-public function create_account()
-{
-  
-}
+  public function create_account(){
+  //go to make_account.php
+  }
 
-public function check_AdminStatus()
-{
+  public function check_AdminStatus(){
 
-}
+  }
 
-public function view_Cart()
-{
+  public function view_Cart(){
   // code...
-}
+  }
 
 }
 
