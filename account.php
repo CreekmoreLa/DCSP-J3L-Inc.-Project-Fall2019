82c0d12865341dbf07382ff771f77796d84ec980
joepatt98 <joepatterson98@icloud.com>
2019-11-18 13:03:39 -0600

    removed account.html and merged its code with account.php

--- account.php
+++ account.php
@@ -1,31 +1,59 @@
-<?php
-    session_start();
- //
-    require_once('login.php');
+<!DOCTYPE HTML>
+<html lang = "en">
 
-    $conn = new mysqli($hn, $un, $pw, $db);
-    if ($conn->connect_error)
-        die($conn->connect_error);
+<head>
 
-    $query = "SELECT * FROM mailing_address WHERE email = "" AND password = """
-    $query2 = "SELECT * FROM username WHERE email = "" AND password = """
-    $query3 = "SELECT * FROM credit_card WHERE email = "" AND password = """
+    <title> Account Page </title>
 
-    session_start();
+</head>
 
-    if ($_SESSION['Logged in as User'] = true)
-    {
-        echo 'Welcome to your Account Page ' . user_name . "!";
-        echo 'Rewards Points: ' . reward_points;
-        echo 'Mailing Address: ' . mail_address;
-        echo 'Credit Card info: ' . cc_num;
-    }
-    elif ($_SESSION['Logged in as Admin'] = true)
-    {
-        echo 'Welcome Admin' . user_name . "!";
-        echo 'Rewards Points: ' . reward_points;
-        echo 'Mailing Address: ' . mail_address;
-        echo 'Credit Card info: ' . cc_num;
-    }
+<body>
 
-?>
+  <input type="button" id="shopping_cart" onclick="document.location.href='Shopping_Cart.php'" value="Shopping Cart">
+
+  <input type="text" id="search_bar" onkeyup="myFunction()" placeholder="Search here...">
+
+
+  <br> <h1> Welcome to the Account Page of J3L's Shirt Shop! </h1> <br>
+
+  <?php
+      session_start();
+
+      require_once('login.php');
+
+      $conn = new mysqli($hn, $un, $pw, $db);
+      if ($conn->connect_error)
+          die($conn->connect_error);
+
+      // TODO: pull from cookie or session...
+      $user_name = ;
+
+      $query = "SELECT email, user_name, cc_num, mail_address, reward_points FROM USERS WHERE user_name = "$user_name"";
+      $result = $conn->query($query);
+
+      while($row = $result->fetch_array()) {
+
+        if ($_SESSION['Logged in as User'] = true)
+        {
+            echo 'Welcome to your Account Page User, ' . $row[user_name] . "!";
+            echo 'Rewards Points: ' . $row[reward_points];
+            echo 'Mailing Address: ' . $row[mail_address];
+            echo 'Credit Card info: ' . $row[cc_num];
+        }
+
+        else if ($_SESSION['Logged in as Admin'] = true)
+        {
+            echo 'Welcome to your Account Page Admin, ' . $row[user_name] . "!";
+            echo 'Rewards Points: ' . $row[reward_points];
+            echo 'Mailing Address: ' . $row[mail_address];
+            echo 'Credit Card info: ' . $row[cc_num];
+        }
+
+      }
+
+  ?>
+
+</body>
+
+
+</html>
