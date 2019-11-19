f72c1b7dba322a0b465bb64dff857f5874c406ee
RushingJ3 <57373613+RushingJ3@users.noreply.github.com>
2019-11-18 20:40:21 -0600

    cookie fixed?

--- account.php
+++ account.php
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-<?php
-    session_start();
- 
-    require_once('login.php');
-
-    $conn = new mysqli($hn, $un, $pw, $db);
-    if ($conn->connect_error)
-        die($conn->connect_error);
-
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
-   
-    $conn = new mysqli($servername, $username, $password);
-
-    $query = "SELECT * FROM mailing_address WHERE email = "" AND password = """
-    $query2 = "SELECT * FROM username WHERE email = "" AND password = """
-    $query3 = "SELECT * FROM credit_card WHERE email = "" AND password = """
-
-    session_start();
-
-    if ($_SESSION['Logged in as User'] = true)
-    {
-        echo 'Welcome to your Account Page ' . user_name . "!";
-        echo 'Rewards Points: ' . reward_points;
-        echo 'Mailing Address: ' . mail_address;
-        echo 'Credit Card info: ' . cc_num; 
-    }
-    elif ($_SESSION['Logged in as Admin'] = true)
-    {
-        echo 'Welcome Admin' . user_name . "!";
-        echo 'Rewards Points: ' . reward_points;
-        echo 'Mailing Address: ' . mail_address;
-        echo 'Credit Card info: ' . cc_num; 
-    }
-    
-?>
-=======
 <!DOCTYPE HTML>
 <html lang = "en">
 
@@ -65,7 +26,9 @@
           die($conn->connect_error);
 
       // TODO: pull from cookie or session...
-      $user_name = "" ;
+      $cookie_name = "user";
+      $cookie_value = user_name;
+      setcookie($cookie_name, $cookie_value, time() + (86400 *30), "/");
 
       $query = "SELECT email, user_name, cc_num, mail_address, reward_points FROM USERS WHERE user_name = '$user_name'";
       $result = $conn->query($query);
@@ -96,4 +59,3 @@
 
 
 </html>
->>>>>>> 6da548762131c867e4ff06093a5fbccb40dd7cfa
