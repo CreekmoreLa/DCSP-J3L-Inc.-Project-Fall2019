f4f95a1f7542c67de2f11903398e38c516d864d1
RushingJ3 <57373613+RushingJ3@users.noreply.github.com>
2019-11-20 12:05:51 -0600

    new database

--- build_tables_on_pluto.sql
+++ build_tables_on_pluto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 17, 2019 at 05:29 AM
+-- Generation Time: Nov 20, 2019 at 06:01 PM
 -- Server version: 10.3.18-MariaDB
 -- PHP Version: 5.4.16
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `netid`
+-- Database: `jr2329`
 --
 
 -- --------------------------------------------------------
@@ -40,36 +40,16 @@ CREATE TABLE IF NOT EXISTS `INVENTORY` (
 --
 
 INSERT INTO `INVENTORY` (`shirtID`, `price`, `quantity`, `size`, `color`, `sleeve`) VALUES
+('000021', 10, 7, 'XXX-Large', 'Black', 0),
+('000069', 12.69, 13, 'XX-Large', 'Teal', 1),
 ('000121', 13.45, 22, 'Large', 'Maroon', 1),
 ('000123', 25.99, 13, 'Small', 'Blue', 0),
+('000154', 19.99, 19, 'Large', 'Purple', 1),
+('000212', 15.99, 15, 'X-Small', 'Orange', 0),
 ('000382', 7.99, 17, 'Medium', 'Green', 0),
-('000567', 6.79, 4, 'Small', 'White', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `USERS`
---
-
-CREATE TABLE IF NOT EXISTS `USERS` (
-  `email` varchar(75) NOT NULL,
-  `user_name` varchar(50) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `cc_num` varchar(25) NOT NULL,
-  `mail_address` varchar(90) NOT NULL,
-  `reward_points` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `USERS`
---
-
-INSERT INTO `USERS` (`email`, `user_name`, `password`, `cc_num`, `mail_address`, `reward_points`) VALUES
-('bossman@msstate.edu', 'The Boss', 'makemeadmin', '123456789', '804 Alexis Ave Starkville, MS 39751', 0),
-('jared@msstate.edu', 'Jared Oakes', 'jared', '123456789', '804 Alexis Ave Starkville, MS 39751', 9),
-('jayden@msstate.edu', 'Jayden Rushing', 'jayden', '123456789', '804 Alexis Ave Starkville, MS 39751', 12),
-('joe@msstate.edu', 'Joe Patterson', 'joe', '123456789', '804 Alexis Ave Starkville, MS 39751', 0),
-('laura@msstate.edu', 'Laura Creekmore', 'laura', '123456789', '804 Alexis Ave Starkville, MS 39751', 9);
+('000412', 21.99, 20, 'X-Large', 'Red', 1),
+('000567', 6.79, 4, 'Small', 'White', 1),
+('042069', 16, 5, 'Medium', 'Yellow', 0);
 
 --
 -- Indexes for dumped tables
@@ -80,12 +60,6 @@ INSERT INTO `USERS` (`email`, `user_name`, `password`, `cc_num`, `mail_address`,
 --
 ALTER TABLE `INVENTORY`
   ADD PRIMARY KEY (`shirtID`);
-
---
--- Indexes for table `USERS`
---
-ALTER TABLE `USERS`
-  ADD PRIMARY KEY (`email`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
