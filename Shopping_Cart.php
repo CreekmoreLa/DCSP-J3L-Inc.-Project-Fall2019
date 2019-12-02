e03d9b1376796776468714442a558c7030eea039
joepatt98 <joepatterson98@icloud.com>
2019-12-01 22:07:21 -0600

    even more bug fixes to the cart (and possibly added more bugs lol)...

--- Shopping_Cart.php
+++ Shopping_Cart.php
@@ -35,66 +35,72 @@
 
 <?php
 
+
+
+  $item_to_add = $_POST["add_to_cart"];
+
+  if (!isset($_SESSION["Cart"]) && $item_to_add != null) {
+    $_SESSION["Cart"] = array($item_to_add);
+    Display_Cart();
+  }
+  else if (isset($_SESSION["Cart"]) && $item_to_add != null) {
+    array_push($_SESSION["Cart"], $item_to_add);
+    Display_Cart();
+  }
+  else if (!$_SESSION['Logged in as User'] || !$_SESSION['Logged in as Admin']) {
+    echo "You must login before you can add items to your cart!";
+  }
+  else if (!isset($_POST["add_to_cart"]) && (empty($_POST["add_to_cart"]))) {
+    echo "You must add to cart first!";
+  }
+
+function Display_Cart() {
+
   require_once('login.php');
 
   $conn = new mysqli($hn, $un, $pw, $db);
   if ($conn->connect_error)
       die($conn->connect_error);
 
-  $item_to_add = $_POST["add_to_cart"];
+  echo '
+  <table>
+    <tr>
+      <th colspan="1">ShirtID</th>
+      <th colspan="1">Price</th>
+      <th colspan="1">Quantity</th>
+      <th colspan="1">Size</th>
+      <th colspan="1">Color</th>
+      <th colspan="1">Sleeve Length</th>
+      <th colspan="1">Purchase?</th>
+    </tr>';
 
-  if (!isset($_SESSION["Cart"]) && $item_to_add != null) {
-    $_SESSION["Cart"] = array($item_to_add);
-  }
+    $arrayLength = count($_SESSION["Cart"]);
+    $i = 0;
+    $cart = $_SESSION["Cart"];
 
-  else if (isset($_SESSION["Cart"]) && $item_to_add != null) {
-    array_push($_SESSION["Cart"], $item_to_add);
-  }
+    while ($i < $arrayLength) {
 
-  else if (!$_SESSION['Logged in as User'] || !$_SESSION['Logged in as Admin']) {
-    echo "You must login before you can add items to your cart!";
-  }
+      $query = "SELECT * FROM `INVENTORY` WHERE shirtID = '$cart[$i]'";
+      $result = $conn->query($query);
+      $row = $result->fetch_array();
 
-  else if (!isset($_POST["add_to_cart"]) && (empty($_POST["add_to_cart"]))) {
-    echo "You must add to cart first!";
-  }
+      echo '
+        <tr>
+          <td>'. $_SESSION["Cart"][$i] . '</td>
+          <td>'. $row['price'] . '</td>
+          <td>'. $row['quantity'] . '</td>
+          <td>'. $row['size'] . '</td>
+          <td>'. $row['color'] . '</td>
+          <td>'. $row['sleeve'] . '</td>
+          <td> <input type="radio" name="purchase_item"> </td>
+        </tr>';
+      $i++;
+    }
 
-    echo '
-    <table>
-      <tr>
-        <th colspan="1">ShirtID</th>
-        <th colspan="1">Price</th>
-        <th colspan="1">Quantity</th>
-        <th colspan="1">Size</th>
-        <th colspan="1">Color</th>
-        <th colspan="1">Sleeve Length</th>
-        <th colspan="1">Purchase?</th>
-      </tr>';
+  echo '</table>';
 
-      $arrayLength = count($_SESSION["Cart"]);
-      $i = 0;
-      $cart = $_SESSION["Cart"];
+}
 
-      while ($i < $arrayLength) {
-
-        $query = "SELECT * FROM `INVENTORY` WHERE shirtID = '$cart[$i]'";
-        $result = $conn->query($query);
-        $row = $result->fetch_array();
-
-        echo '
-          <tr>
-            <td>'. $_SESSION["Cart"][$i] . '</td>
-            <td>'. $row['price'] . '</td>
-            <td>'. $row['quantity'] . '</td>
-            <td>'. $row['size'] . '</td>
-            <td>'. $row['color'] . '</td>
-            <td>'. $row['sleeve'] . '</td>
-            <td> <input type="radio" name="purchase_item"> </td>
-          </tr>';
-        $i++;
-      }
-
-    echo '</table>';
 
     $init_var = true;
 
@@ -119,7 +125,7 @@
 
 ?>
 
-    <br> <input type="submit" id="submit" value="Purchase"> <br>
+  <br> <input type="submit" id="submit" value="Purchase"> <br>
 
   </form>
 
